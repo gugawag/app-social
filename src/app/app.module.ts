9bc2deebec8735148ecd0311e461249bc89234eb
Gustavo Wagner <gugawag@gmail.com>
2017-11-19 00:49:09 -0300

    Atualizando para Angular Material e Angular 5, bem como definindo o tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule,
+    MatInputModule, MatButtonModule, MatCheckboxModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
